8f830c89d8614f8aaf9b39f11e891fdf8111d7dc
hansgarcia <79774388+hansgarcia@users.noreply.github.com>
2024-05-20 21:31:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Dom } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PuppyList from './components/PuppyList.jsx';
 import PuppyDetails from './components/PuppyDetails.jsx';
 import PuppyForm from './components/PuppyForm.jsx';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={PuppyList} />
-        <Route path="/puppy/:id" component={PuppyDetails} />
-        <Route path="/new" component={PuppyForm} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" exact component={PuppyList} />
+      <Route path="/puppy/:id" component={PuppyDetails} />
+      <Route path="/new" component={PuppyForm} />
+    </Switch>
   );
 }
 
