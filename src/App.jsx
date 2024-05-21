2623aa3f8b91755f2fc3e9283511adad795b6347
hansgarcia <79774388+hansgarcia@users.noreply.github.com>
2024-05-20 21:08:59 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Dom } from 'react-router-dom';
 import PuppyList from './components/PuppyList.jsx';
 import PuppyDetails from './components/PuppyDetails.jsx';
 import PuppyForm from './components/PuppyForm.jsx';
